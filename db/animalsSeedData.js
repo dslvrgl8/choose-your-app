1b5912ae028e2269a9514c393f98d324a8b0a70c
Dylan Silverglate <dylansilverglate@Dylans-Air.lan>
2023-04-03 12:28:28 -0700

    modules and data added

---
+++ db/animalsSeedData.js
@@ -0,0 +1,7 @@
+module.exports = [
+   { species: 'Dodo Bird', extinct: true, location: "Mauritius", lifeExpectancy: 30}
+   { species: 'Shoebill', extinct: false, location: "Africa", lifeExpectancy: 36}
+   { species: 'Dodo Bird', extinct: true, location: "Mauritius", lifeExpectancy: 30}
+   { species: 'Dodo Bird', extinct: true, location: "Mauritius", lifeExpectancy: 30}
+   { species: 'Dodo Bird', extinct: true, location: "Mauritius", lifeExpectancy: 30}
+]
